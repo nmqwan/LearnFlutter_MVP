eb63ef1eba456cbc1cbe42d2f5bc1c46f57c3fb1
quannm <nguyenminhquan@outlook.com>
2018-09-05 22:01:15 +0700

    1

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("FLUTTER MVP"),
-      ),
-      body: ContactList(),
-    );
+    return ContactsPage();
   }
 }
